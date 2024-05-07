1b0d2d8b7fe0ef6a2482bc746f86efe4a6c629a2
nguyen <125894255+glexiv808@users.noreply.github.com>
2024-05-08 00:48:45 +0700

    Layout Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,10 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+//    buildFeatures {
+//        dataBinding true
+//    }
+
 
     buildTypes {
         release {
@@ -40,4 +44,26 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("com.squareup.retrofit2:retrofit2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-rxjava2:2.6.1")
+
+    implementation("io.reactive.rxjava2:rxandroid:2.0.1")
+
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    implementation("com.makeramen:roundedimageview:2.3.0")
+
+
 }
